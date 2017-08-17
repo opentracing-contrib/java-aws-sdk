24222a0fc43ce648bfde8c240fbb3bf06b612cf7
Sergei Malafeev <sergeymalafeev@gmail.com>
2017-08-17 11:32:24 +0800

    add more tests

--- src/test/java/io/opentracing/contrib/aws/TracingRequestHandlerTest.java
+++ src/test/java/io/opentracing/contrib/aws/TracingRequestHandlerTest.java
@@ -1,6 +1,8 @@
 package io.opentracing.contrib.aws;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
@@ -14,19 +16,20 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.local.main.ServerRunner;
 import com.amazonaws.services.dynamodbv2.local.server.DynamoDBProxyServer;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
 import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
 import com.amazonaws.services.dynamodbv2.model.CreateTableResult;
 import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
 import com.amazonaws.services.dynamodbv2.model.KeyType;
-import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import io.opentracing.ActiveSpan;
 import io.opentracing.mock.MockSpan;
 import io.opentracing.mock.MockTracer;
 import io.opentracing.tag.Tags;
+import io.opentracing.util.ThreadLocalActiveSpanSource;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -34,9 +37,9 @@ import org.junit.Test;
 
 public class TracingRequestHandlerTest {
 
-  private static final MockTracer mockTracer = new MockTracer(MockTracer.Propagator.TEXT_MAP);
+  private static final MockTracer mockTracer = new MockTracer(new ThreadLocalActiveSpanSource(),
+      MockTracer.Propagator.TEXT_MAP);
   private DynamoDBProxyServer server;
-
 
   @Before
   public void before() throws Exception {
@@ -54,42 +57,101 @@ public class TracingRequestHandlerTest {
   }
 
   @Test
-  public void twoRequests() throws Exception {
+  public void two_requests() throws Exception {
     AmazonDynamoDB dbClient = buildClient();
-    firstRequest(dbClient);
-    secondRequest(dbClient);
+    createTable(dbClient, "twoRequests-1");
+    createTable(dbClient, "twoRequests-2");
 
-    List<MockSpan> mockSpans = mockTracer.finishedSpans();
-    assertEquals(2, mockSpans.size());
+    List<MockSpan> spans = mockTracer.finishedSpans();
+    assertEquals(2, spans.size());
 
-    checkSpans(mockSpans);
+    checkSpans(spans);
+    assertNotEquals(spans.get(0).context().traceId(), spans.get(1).context().traceId());
+
     assertNull(mockTracer.activeSpan());
   }
 
   @Test
-  public void twoAsyncRequests() {
+  public void two_requests_with_parent() {
+    AmazonDynamoDB dbClient = buildClient();
+
+    try (ActiveSpan parent = mockTracer.buildSpan("parent-sync").startActive()) {
+      createTable(dbClient, "with-parent-1");
+      createTable(dbClient, "with-parent-2");
+    }
+
+    List<MockSpan> spans = mockTracer.finishedSpans();
+    assertEquals(3, spans.size());
+
+    MockSpan parent = getByOperationName(spans, "parent-sync");
+    assertNotNull(parent);
+
+    for (MockSpan span : spans) {
+      if (parent.operationName().equals(span.operationName())) {
+        continue;
+      }
+      assertEquals(parent.context().traceId(), span.context().traceId());
+      assertEquals(parent.context().spanId(), span.parentId());
+    }
+
+    assertNull(mockTracer.activeSpan());
+  }
+
+  /**
+   * In case of async requests parent-child relation is not created. There is no way to fix that
+   * except explicitly set parent context in TracingRequestHandler
+   */
+  @Test
+  public void async_requests_with_parent() throws Exception {
     AmazonDynamoDBAsync dbClient = buildAsyncClient();
-    Future<ListTablesResult> listTablesResultFuture = firstRequestAsync(dbClient);
-    Future<CreateTableResult> createTableResultFuture = secondRequestAsync(dbClient);
 
-    try {
-      ListTablesResult result = listTablesResultFuture.get(10, TimeUnit.SECONDS);
-      assertTrue(result.getTableNames().size() >= 0);
-    } catch (InterruptedException | ExecutionException | TimeoutException e) {
-      e.printStackTrace();
+    try (ActiveSpan parent = mockTracer.buildSpan("parent-async").startActive()) {
+      Future<CreateTableResult> createTableResultFuture = createTableAsync(dbClient,
+          "with-async-parent-1");
+      Future<CreateTableResult> createTableResultFuture2 = createTableAsync(dbClient,
+          "with-async-parent-2");
+
+      createTableResultFuture.get(10, TimeUnit.SECONDS);
+      createTableResultFuture2.get(10, TimeUnit.SECONDS);
+
+      createTableAsync(dbClient, "with-async-parent-3").get(10, TimeUnit.SECONDS);
     }
 
-    try {
-      CreateTableResult result = createTableResultFuture.get(10, TimeUnit.SECONDS);
-      assertEquals("test", result.getTableDescription().getTableName());
-    } catch (InterruptedException | ExecutionException | TimeoutException e) {
-      e.printStackTrace();
+    List<MockSpan> spans = mockTracer.finishedSpans();
+    assertEquals(4, spans.size());
+
+    MockSpan parent = getByOperationName(spans, "parent-async");
+    assertNotNull(parent);
+
+    for (MockSpan span : spans) {
+      if (parent.operationName().equals(span.operationName())) {
+        continue;
+      }
+      assertNotEquals(parent.context().traceId(), span.context().traceId());
+      assertNotEquals(parent.context().spanId(), span.parentId());
     }
+  }
 
-    List<MockSpan> mockSpans = mockTracer.finishedSpans();
-    assertEquals(2, mockSpans.size());
+  @Test
+  public void two_async_requests() throws Exception {
+    AmazonDynamoDBAsync dbClient = buildAsyncClient();
 
-    checkSpans(mockSpans);
+    Future<CreateTableResult> createTableResultFuture = createTableAsync(dbClient,
+        "twoAsyncRequests-1");
+    Future<CreateTableResult> createTableResultFuture2 = createTableAsync(dbClient,
+        "twoAsyncRequests-2");
+
+    CreateTableResult result = createTableResultFuture.get(10, TimeUnit.SECONDS);
+    assertEquals("twoAsyncRequests-1", result.getTableDescription().getTableName());
+
+    CreateTableResult result2 = createTableResultFuture2.get(10, TimeUnit.SECONDS);
+    assertEquals("twoAsyncRequests-2", result2.getTableDescription().getTableName());
+
+    List<MockSpan> spans = mockTracer.finishedSpans();
+    assertEquals(2, spans.size());
+
+    checkSpans(spans);
+    assertNotEquals(spans.get(0).context().traceId(), spans.get(1).context().traceId());
     assertNull(mockTracer.activeSpan());
   }
 
@@ -127,37 +189,42 @@ public class TracingRequestHandlerTest {
         .withRequestHandlers(new TracingRequestHandler(mockTracer)).build();
   }
 
-  private void firstRequest(AmazonDynamoDB dbClient) {
-    try {
-      ListTablesResult result = dbClient.listTables();
-      assertEquals(0, result.getTableNames().size());
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+  private void createTable(AmazonDynamoDB dbClient, String tableName) {
+    String partitionKeyName = tableName + "Id";
 
-  private Future<ListTablesResult> firstRequestAsync(AmazonDynamoDBAsync dbClient) {
-    return dbClient.listTablesAsync();
-  }
-
-  private void secondRequest(AmazonDynamoDB dbClient) {
     try {
-      CreateTableRequest createTableRequest = new CreateTableRequest().withTableName("test")
+      CreateTableRequest createTableRequest = new CreateTableRequest()
+          .withTableName(tableName)
           .withKeySchema(new KeySchemaElement()
-              .withAttributeName("testId")
-              .withKeyType(KeyType.HASH));
+              .withAttributeName(partitionKeyName)
+              .withKeyType(KeyType.HASH))
+          .withAttributeDefinitions(new AttributeDefinition()
+              .withAttributeName(partitionKeyName).withAttributeType("S"))
+          .withProvisionedThroughput(new ProvisionedThroughput()
+              .withReadCapacityUnits(10L)
+              .withWriteCapacityUnits(5L));
+
       dbClient.createTable(createTableRequest);
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
-  private Future<CreateTableResult> secondRequestAsync(AmazonDynamoDBAsync dbClient) {
+  private Future<CreateTableResult> createTableAsync(AmazonDynamoDBAsync dbClient,
+      String tableName) {
+    String partitionKeyName = tableName + "Id";
 
-    CreateTableRequest createTableRequest = new CreateTableRequest().withTableName("test")
+    CreateTableRequest createTableRequest = new CreateTableRequest()
+        .withTableName(tableName)
         .withKeySchema(new KeySchemaElement()
-            .withAttributeName("testId")
-            .withKeyType(KeyType.HASH));
+            .withAttributeName(partitionKeyName)
+            .withKeyType(KeyType.HASH))
+        .withAttributeDefinitions(new AttributeDefinition()
+            .withAttributeName(partitionKeyName).withAttributeType("S"))
+        .withProvisionedThroughput(new ProvisionedThroughput()
+            .withReadCapacityUnits(10L)
+            .withWriteCapacityUnits(5L));
+
     return dbClient.createTableAsync(createTableRequest,
         new AsyncHandler<CreateTableRequest, CreateTableResult>() {
           @Override
@@ -170,5 +237,14 @@ public class TracingRequestHandlerTest {
 
           }
         });
+  }
+
+  private MockSpan getByOperationName(List<MockSpan> spans, String operationName) {
+    for (MockSpan span : spans) {
+      if (operationName.equals(span.operationName())) {
+        return span;
+      }
+    }
+    return null;
   }
 }
